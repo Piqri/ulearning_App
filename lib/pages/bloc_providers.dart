d1928c9fc6d8b6e410478104c37a2a943bab42fd
M. Al Piqri <mhmmdalfkr026@gmail.com>
2023-07-31 15:40:25 +0700

    bloc provider

--- lib/pages/bloc_providers.dart
+++ lib/pages/bloc_providers.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:ulearning_app/app_blocs.dart';
+// import 'package:ulearning_app/app_blocs.dart';
 import 'package:ulearning_app/pages/register/bloc/register_blocs.dart';
 import 'package:ulearning_app/pages/sign_in/bloc/sign_in_blocs.dart';
 import 'package:ulearning_app/pages/welcome/bloc/welcome_blocs.dart';
@@ -7,7 +7,8 @@ import 'package:ulearning_app/pages/welcome/bloc/welcome_blocs.dart';
 class AppBlocProviders {
   static get allBlocProviders => [
         BlocProvider(
-          lazy: false, create: (context) => WelcomeBloc(),
+          lazy: false,
+          create: (context) => WelcomeBloc(),
         ),
         // BlocProvider(
         //   create: (context) => AppBlocs(),
